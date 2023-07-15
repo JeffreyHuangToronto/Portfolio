a4c473bc390fc4848b1e20908a3f1389637d654e
Jeffrey Huang <jeffreyh.huang@mail.utoronto.ca>
2023-07-15 16:25:27 -0400

    Added some content to Projects

---
+++ components/Project.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="project">
+        <div class="project_header">
+            <h2 class="project_header__title">
+                <slot name="title" />
+            </h2>
+            <h4 class="project_header__date"><slot name="date" /></h4>
+        </div>
+        <p class="project__description"><slot name="description" /></p>
+        <div class="project__link"></div>
+        <slot name="link" />
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    github: String,
+});
+</script>
+
+<style lang="scss">
+.project {
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    align-items: flex-start;
+    @media screen and (max-width: 900px) {
+        // margin-left: 50px;
+        padding-left: 50px;
+        padding-right: 50px;
+        // width: 100%;
+        min-width: min-content;
+    }
+
+    &_header {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        gap: 25px;
+        &__title {
+        }
+        &__date {
+        }
+    }
+    &_link {
+    }
+}
+</style>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,1 +1,65 @@
-<template></template>
+<template>
+    <div class="projects">
+        <Project>
+            <template #title> Sheep Of The Four Kingdoms </template>
+            <template #date> May 2020 – June 2020 </template>
+            <template #description>
+                My brother and I joined the UTSC Game Jam Summer Series I,
+                creating a game using Unity.
+            </template>
+            <template #link>
+                <NuxtLink
+                    to="https://itch.io/jam/utsc-summer-series-i-2020/rate/670435"
+                    target="_blank"
+                    >Link to project</NuxtLink
+                ></template
+            >
+        </Project>
+        <Project>
+            <template #title> BooksWeeR</template>
+            <template #date> Feb 2020 – Feb 2020</template>
+            <template #description>
+                This is the first Hackathon I had attended, this project was
+                made using Python and runs in the terminal as our team wasn't
+                experienced in connecting the backend to the front end. As a
+                result I learnt that there was still many things I could learn
+                in the future to add to my repertoire.
+            </template>
+            <template #link>
+                <NuxtLink
+                    to="https://devpost.com/software/booksweer"
+                    target="_blank"
+                    >Link to project</NuxtLink
+                ></template
+            >
+        </Project>
+        <Project>
+            <template #title> MinBatTon</template>
+            <template #date> Feb 14, 2020 – Feb 24, 2020</template>
+            <template #description>
+                MinBatTon is a game for playing badminton, inspired by our love
+                for badminton as a sport. After creating this game, we improved
+                understanding of Unity’s UI system and also learnt how to
+                manipulate UI from scripts
+            </template>
+            <template #link>
+                <NuxtLink
+                    to="https://devpost.com/software/booksweer"
+                    target="_blank"
+                    >Link to project</NuxtLink
+                ></template
+            >
+        </Project>
+    </div>
+</template>
+
+<style lang="scss">
+.projects {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    gap: 25px;
+}
+</style>
